12c5fc2f96be26ea59b0e5eab0bf440f9a25c882
leandrofavila <leandrofavila@gmail.com>
2024-03-22 10:33:21 -0300

    Initial commit

---
+++ QR_Reader.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import datetime
+import time
+import os
+import numpy as np
+from threading import Thread
+
+global capture, rec_frame, grey, switch, neg, rec, out
+capture = 0
+grey = 0
+neg = 0
+switch = 0
+rec = 0
+
+# make shots directory to save pics
+try:
+    os.mkdir('./shots')
+except OSError as error:
+    pass
+
+
+app = Flask(__name__, template_folder='./templates')
+
+camera = cv2.VideoCapture(1)
+
+
+
+def record(out_):
+    global rec_frame
+    while rec:
+        time.sleep(0.05)
+        out_.write(rec_frame)
+
+
+def gen_frames():
+    global out, capture, rec_frame
+    while True:
+        success, frame = camera.read()
+        if success:
+            if grey:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if neg:
+                frame = cv2.bitwise_not(frame)
+            if capture:
+                capture = 0
+                now = datetime.datetime.now()
+                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":", ''))])
+                cv2.imwrite(p, frame)
+
+            if rec:
+                rec_frame = frame
+                frame = cv2.putText(cv2.flip(frame, 1), "Recording...", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                    (0, 0, 255), 4)
+                frame = cv2.flip(frame, 1)
+
+            try:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                print(e)
+                pass
+
+        else:
+            pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/requests', methods=['POST', 'GET'])
+def tasks():
+    print("Form data:", request.form)
+    global switch, camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture = 1
+            print("Capture button pressed. Capture set to 1.")
+        elif request.form.get('grey') == 'Grey':
+            global grey
+            grey = not grey
+        elif request.form.get('neg') == 'Negative':
+            global neg
+            neg = not neg
+        elif request.form.get('stop') == 'Stop/Start':
+
+            if switch == 1:
+                switch = 0
+                camera.release()
+                cv2.destroyAllWindows()
+
+            else:
+                camera = cv2.VideoCapture(0)
+                switch = 1
+        elif request.form.get('rec') == 'Start/Stop Recording':
+            global rec, out
+            rec = not rec
+            if rec:
+                now = datetime.datetime.now()
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":", '')), fourcc, 20.0, (640, 480))
+                # Start new thread for recording the video
+                thread = Thread(target=record, args=[out, ])
+                thread.start()
+            elif not rec:
+                out.release()
+
+
+    elif request.method == 'GET':
+        return render_template('index.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='10.40.3.48', port=5001)
+
+camera.release()
+cv2.destroyAllWindows()
